f51b685b78aff8dbeca9bc7878bc0a271d3f1916
Anish Balabattuni <anishbalabattuni.cs24@bmsce.ac.in>
2026-03-02 12:33:24 +0530

    Program 1 Output added

--- Program1-FCFC 02_03_2026/Program 1 - FCFS.c
+++ Program1-FCFC 02_03_2026/Program 1 - FCFS.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    int n, bt[20], wt[20], tat[20], at[20], ct[20];
-    int i;
+    int n, bt[20], wt[20], tat[20], at[20], ct[20], p[20];
+    int i, j, temp;
     float twt = 0.0, ttat = 0.0, awt, att;
 
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
+        p[i] = i + 1;
+
         printf("Enter Arrival Time for Process %d: ", i + 1);
         scanf("%d", &at[i]);
 
         printf("Enter Burst Time for Process %d: ", i + 1);
         scanf("%d", &bt[i]);
+    }
+
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (at[i] > at[j]) {
+
+                temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
     }
 
     ct[0] = at[0] + bt[0];
@@ -43,7 +65,7 @@ int main() {
     printf("\nPROCESS\tAT\tBT\tCT\tWT\tTAT");
     for (i = 0; i < n; i++) {
         printf("\nP%d\t%d\t%d\t%d\t%d\t%d",
-               i + 1, at[i], bt[i], ct[i], wt[i], tat[i]);
+               p[i], at[i], bt[i], ct[i], wt[i], tat[i]);
     }
 
     printf("\n\nAverage Waiting Time = %.2f", awt);
